ec87e7f32856ae88f822d59eb13a173cdc9b6b1a
Natalia Titova <bnmlucky@gmail.com>
2020-06-15 11:24:03 -0400

    rewrote an if statement as a ternary

--- two.js
+++ two.js
@@ -1,7 +1,14 @@
-var isTrue = false;
+var isTrue = true;
 
-if (isTrue) {
-	console.log('yes');
-} else {
-	console.log('no');
-}
+// if (isTrue) {
+// 	console.log('yes');
+// } else {
+// 	console.log('no');
+// }
+
+var x = 2 + 5;
+
+var yesOrNo = isTrue ? 'yes' : 'no';
+console.log(yesOrNo);
+
+// ternary should be used for very simple cases where the intent is unmistakable and obvious
